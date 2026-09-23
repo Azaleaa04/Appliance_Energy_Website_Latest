4983cc4aa3d348235a01386a8c1d03e4942359cc
Azaleaa04 <namandagenevieveheally@gmail.com>
2026-09-24 07:53:58 +0800

    Test

---
+++ main.js
@@ -0,0 +1,4 @@
+// Highlight current page dynamically or add interaction
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Appliance Energy Consumption website loaded successfully.");
+});
